f340d94e18e088e4187501823ae75a7ab2693375
Tod Fox <tod@fox.as>
2020-11-01 18:20:56 -0600

    fixin

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,7 +98,33 @@ $(window).on('load', function () {
 	}
 	/* ########################################### /hero parallax ############################################## */
 
-	// testimonial-slider
+	
+  // skill count
+  $(".process__progress").waypoint(
+    function () {
+      $(".progress-value span").each(function () {
+        $(this)
+          .prop("Counter", 0)
+          .animate(
+            {
+              Counter: $(this).text(),
+            },
+            {
+              duration: 3000,
+              easing: "swing",
+              step: function (now) {
+                $(this).text(Math.ceil(now));
+              },
+            }
+          );
+      });
+      $(".process__progress_item").addClass("js-animation");
+      this.destroy();
+    },
+    { offset: "80%" }
+  );
+
+        // testimonial-slider
 	$('.testimonial-slider').slick({
 		dots: true,
 		infinite: true,
